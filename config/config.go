f9977c1a233488a1848f4c11a5dbdc1776648bd9
Gen1us2k <minkin.andrew@gmail.com>
2017-11-27 18:17:21 +0600

    #18 Added configuration with env variables

--- config/config.go
+++ config/config.go
@@ -1,6 +1,19 @@
 package config
 
+import "github.com/kelseyhightower/envconfig"
+
 type (
-	// Config interface used for configuration via multiple backends(ENV, yaml/json files)
-	Config interface{}
+	// Config struct used for configuration of app with env variables
+	Config struct {
+		SlackToken string `envconfig:"SLACK_TOKEN"`
+	}
 )
+
+// Get method processes env variables and fills Config struct
+func Get() (Config, error) {
+	var c Config
+	if err := envconfig.Process("comedian", &c); err != nil {
+		return c, err
+	}
+	return c, nil
+}
---
+++ config/config_test.go
@@ -0,0 +1,15 @@
+package config
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestConfig(t *testing.T) {
+	os.Setenv("SLACK_TOKEN", "Hey_token")
+	conf, err := Get()
+	assert.NoError(t, err)
+	assert.Equal(t, conf.SlackToken, "Hey_token")
+}
