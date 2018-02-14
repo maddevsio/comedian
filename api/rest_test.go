696baa46eaa4fb184dc6c2b932b48657d88f733a
malikim <kim.malika.v@gmail.com>
2018-02-14 12:26:00 +0600

    fixed test

--- api/rest_test.go
+++ api/rest_test.go
@@ -90,7 +90,7 @@ func TestHandleCommands(t *testing.T) {
 
 	if assert.NoError(t, rest.handleCommands(context)) {
 		assert.Equal(t, http.StatusOK, rec.Code)
-		assert.Equal(t, "standup time at 17:08 (UTC) added", rec.Body.String())
+		assert.Equal(t, "standup time at 12:10 (UTC) added", rec.Body.String())
 	}
 
 	//add time empty text
