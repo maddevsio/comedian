610a4d23c1f987551241e648b3aa51a89ff70e32
malikim <kim.malika.v@gmail.com>
2018-02-19 17:20:25 +0600

    #fixed adding time test

--- api/rest_test.go
+++ api/rest_test.go
@@ -115,7 +115,8 @@ func TestHandleCommands(t *testing.T) {
 
 	if assert.NoError(t, rest.handleCommands(context)) {
 		assert.Equal(t, http.StatusOK, rec.Code)
-		assert.Equal(t, "standup time at 12:10 (UTC) added", rec.Body.String())
+		assert.Equal(t, "standup time at 12:10 (UTC) added, but there is no standup users " +
+			"for this channel", rec.Body.String())
 	}
 
 	//add time empty text
