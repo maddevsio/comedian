59610068a4932dc72a29fceb6b50d21a7099495d
anatoliy <fedorenko.tolik@gmail.com>
2018-06-25 17:20:44 +0600

    complete config tests

--- config/config_test.go
+++ config/config_test.go
@@ -8,11 +8,23 @@ import (
 )
 
 func TestConfig(t *testing.T) {
-	os.Setenv("SLACK_TOKEN", "Hey_token")
+	os.Setenv("COMEDIAN_SLACK_TOKEN", "token")
+	os.Setenv("COMEDIAN_DATABASE", "DB")
+	os.Setenv("COMEDIAN_HTTP_BIND_ADDR", "0.0.0.0:8080")
+	os.Setenv("COMEDIAN_NOTIFIER_CHECK_INTERVAL", "15")
+	os.Setenv("COMEDIAN_MANAGER", "manager")
+	os.Setenv("COMEDIAN_DIRECT_MANAGER_CHANNEL_ID", "someID")
+	os.Setenv("COMEDIAN_REPORT_TIME", "17:00")
+	os.Setenv("COMEDIAN_DEBUG", "true")
+
 	conf, err := Get()
 	assert.NoError(t, err)
-	assert.Equal(t, conf.SlackToken, "Hey_token")
-	os.Setenv("COMEDIAN_DEBUG", "string")
-	conf, err = Get()
-	assert.Error(t, err)
+	assert.Equal(t, conf.SlackToken, "token")
+	assert.Equal(t, conf.DatabaseURL, "DB")
+	assert.Equal(t, conf.HTTPBindAddr, "0.0.0.0:8080")
+	assert.Equal(t, conf.NotifierCheckInterval, uint64(15))
+	assert.Equal(t, conf.Manager, "manager")
+	assert.Equal(t, conf.DirectManagerChannelID, "someID")
+	assert.Equal(t, conf.ReportTime, "17:00")
+	assert.Equal(t, conf.Debug, true)
 }
