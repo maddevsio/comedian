4d529410b4ccfa037b3e41b0fbac318be857e4d2
Anatoliy Fedorenko <fedorenko.tolik@gmail.com>
2018-07-02 12:18:41 +0600

    Modified .gitignore to ignore .html test files

--- reporting/reporting_test.go
+++ reporting/reporting_test.go
@@ -126,77 +126,74 @@ func TestStandupReportByProject(t *testing.T) {
 	assert.NoError(t, db.DeleteStandupUserByUsername(user2.SlackName, user2.ChannelID))
 }
 
-// func TestStandupReportByUser(t *testing.T) {
-// 	c, err := config.Get()
-// 	assert.NoError(t, err)
-// 	db, err := storage.NewMySQL(c)
-// 	assert.NoError(t, err)
+func TestStandupReportByUser(t *testing.T) {
+	c, err := config.Get()
+	assert.NoError(t, err)
+	db, err := storage.NewMySQL(c)
+	assert.NoError(t, err)
 
-// 	channelID := "QWERTY123"
-// 	channelName := "chanName"
-// 	user, err := db.CreateStandupUser(model.StandupUser{
-// 		SlackUserID: "userID1",
-// 		SlackName:   "user1",
-// 		FullName:    "",
-// 		ChannelID:   channelID,
-// 		Channel:     channelName,
-// 	})
-// 	assert.NoError(t, err)
-// 	dateFrom, err := time.Parse("2006-01-02", "2018-06-24")
-// 	assert.NoError(t, err)
-// 	dateTo, err := time.Parse("2006-01-02", "2018-06-28")
-// 	assert.NoError(t, err)
-// 	text, err := StandupReportByUser(db, user, dateFrom, dateTo)
-// 	assert.NoError(t, err)
-// 	assert.Equal(t, "Full Standup Report for user <@user1>:\n\n\n\nReport from 2018-06-28 to 2018-06-29:\n\n<@user1>: ignored standup\n", text)
+	channelID := "QWERTY123"
+	channelName := "chanName"
+	user, err := db.CreateStandupUser(model.StandupUser{
+		SlackUserID: "userID1",
+		SlackName:   "user1",
+		ChannelID:   channelID,
+		Channel:     channelName,
+	})
+	assert.NoError(t, err)
+	dateFrom, err := time.Parse("2006-01-02", "2018-06-24")
+	assert.NoError(t, err)
+	dateTo, err := time.Parse("2006-01-02", "2018-06-28")
+	assert.NoError(t, err)
+	text, err := StandupReportByUser(db, user, dateFrom, dateTo)
+	assert.NoError(t, err)
+	assert.Equal(t, "Full Standup Report for user <@user1>:\n\n\n\nReport from 2018-06-28 to 2018-06-29:\n\n<@user1>: ignored standup\n", text)
 
-// 	standup1, err := db.CreateStandup(model.Standup{
-// 		ChannelID:  channelID,
-// 		Comment:    "my standup",
-// 		UsernameID: "userID1",
-// 		Username:   "user1",
-// 		MessageTS:  "123",
-// 	})
-// 	text, err = StandupReportByUser(db, user, dateFrom, dateTo)
-// 	assert.NoError(t, err)
-// 	assert.Equal(t, "Full Standup Report for user <@user1>:\n\n\n\nReport from 2018-06-28 to 2018-06-29:\n\nOn project: <#QWERTY123>\nmy standup\n", text)
-// 	assert.NoError(t, db.DeleteStandup(standup1.ID))
+	standup1, err := db.CreateStandup(model.Standup{
+		ChannelID:  channelID,
+		Comment:    "my standup",
+		UsernameID: "userID1",
+		Username:   "user1",
+		MessageTS:  "123",
+	})
+	text, err = StandupReportByUser(db, user, dateFrom, dateTo)
+	assert.NoError(t, err)
+	assert.Equal(t, "Full Standup Report for user <@user1>:\n\n\n\nReport from 2018-06-28 to 2018-06-29:\n\nOn project: <#QWERTY123>\nmy standup\n", text)
+	assert.NoError(t, db.DeleteStandup(standup1.ID))
 
-// 	assert.NoError(t, db.DeleteStandupUserByUsername(user.SlackName, user.ChannelID))
-// }
+	assert.NoError(t, db.DeleteStandupUserByUsername(user.SlackName, user.ChannelID))
+}
 
-// func TestStandupReportByProjectAndUser(t *testing.T) {
-// 	c, err := config.Get()
-// 	assert.NoError(t, err)
-// 	db, err := storage.NewMySQL(c)
-// 	assert.NoError(t, err)
+func TestStandupReportByProjectAndUser(t *testing.T) {
+	c, err := config.Get()
+	assert.NoError(t, err)
+	db, err := storage.NewMySQL(c)
+	assert.NoError(t, err)
 
-// 	channelID := "QWERTY123"
-// 	channelName := "chanName"
-// 	user1, err := db.CreateStandupUser(model.StandupUser{
-// 		SlackUserID: "userID1",
-// 		SlackName:   "user1",
-// 		FullName:    "",
-// 		ChannelID:   channelID,
-// 		Channel:     channelName,
-// 	})
-// 	assert.NoError(t, err)
-// 	dateFrom, err := time.Parse("2006-01-02", "2018-06-24")
-// 	assert.NoError(t, err)
-// 	dateTo, err := time.Parse("2006-01-02", "2018-06-28")
-// 	assert.NoError(t, err)
-// 	text, err := StandupReportByProjectAndUser(db, channelName, user1, dateFrom, dateTo)
-// 	assert.NoError(t, err)
-// 	assert.Equal(t, "Standup Report Project: chanName, User: <@user1>\n\n\n\nReport from 2018-06-28 to 2018-06-29:\n\n<@user1> ignored standup!\n", text)
+	channelID := "QWERTY123"
+	channelName := "chanName"
+	user1, err := db.CreateStandupUser(model.StandupUser{
+		SlackUserID: "userID1",
+		SlackName:   "user1",
+		ChannelID:   channelID,
+		Channel:     channelName,
+	})
+	assert.NoError(t, err)
+	dateFrom, err := time.Parse("2006-01-02", "2018-06-24")
+	assert.NoError(t, err)
+	dateTo, err := time.Parse("2006-01-02", "2018-06-28")
+	assert.NoError(t, err)
+	text, err := StandupReportByProjectAndUser(db, channelName, user1, dateFrom, dateTo)
+	assert.NoError(t, err)
+	assert.Equal(t, "Standup Report Project: chanName, User: <@user1>\n\n\n\nReport from 2018-06-28 to 2018-06-29:\n\n<@user1> ignored standup!\n", text)
 
-// 	user2, err := db.CreateStandupUser(model.StandupUser{
-// 		SlackUserID: "userID2",
-// 		SlackName:   "user2",
-// 		FullName:    "",
-// 		ChannelID:   channelID,
-// 		Channel:     channelName,
-// 	})
-// 	assert.NoError(t, err)
-// 	assert.NoError(t, db.DeleteStandupUserByUsername(user1.SlackName, user1.ChannelID))
-// 	assert.NoError(t, db.DeleteStandupUserByUsername(user2.SlackName, user2.ChannelID))
-// }
+	user2, err := db.CreateStandupUser(model.StandupUser{
+		SlackUserID: "userID2",
+		SlackName:   "user2",
+		ChannelID:   channelID,
+		Channel:     channelName,
+	})
+	assert.NoError(t, err)
+	assert.NoError(t, db.DeleteStandupUserByUsername(user1.SlackName, user1.ChannelID))
+	assert.NoError(t, db.DeleteStandupUserByUsername(user2.SlackName, user2.ChannelID))
+}
