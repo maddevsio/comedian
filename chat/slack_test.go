39027781d91da7f07520601a22be27b06dfba60e
Anatoliy Fedorenko <fedorenko.tolik@gmail.com>
2018-07-03 18:27:33 +0600

    Fix tests

--- chat/slack_test.go
+++ chat/slack_test.go
@@ -21,3 +21,33 @@ func TestCleanMessage(t *testing.T) {
 	assert.Equal(t, "What's up?", text)
 	assert.False(t, ok)
 }
+
+func TestSendMessage(t *testing.T) {
+	c, err := config.Get()
+	assert.NoError(t, err)
+	s, err := NewSlack(c)
+	assert.NoError(t, err)
+	err = s.SendMessage(c.DirectManagerChannelID, "MSG to manager!")
+	assert.NoError(t, err)
+}
+
+// func TestSendUserMessage(t *testing.T) {
+// 	c, err := config.Get()
+// 	assert.NoError(t, err)
+// 	s, err := NewSlack(c)
+// 	assert.NoError(t, err)
+
+// 	su1, err := s.db.CreateStandupUser(model.StandupUser{
+// 		SlackUserID: "userID1",
+// 		SlackName:   "user1",
+// 		ChannelID:   "123qwe",
+// 		Channel:     "channel1",
+// 	})
+// 	assert.NoError(t, err)
+
+// 	err = s.SendUserMessage("userID1", "MSG to manager!")
+// 	assert.NoError(t, err)
+
+// 	assert.NoError(t, s.db.DeleteStandupUserByUsername(su1.SlackName, su1.ChannelID))
+
+// }
