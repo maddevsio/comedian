e59222eaec1c972303d3030296c30ddfc8a82d33
Anatoliy Fedorenko <fedorenko.tolik@gmail.com>
2018-08-30 17:21:55 +0600

    Add default values to env variables

--- config/config.go
+++ config/config.go
@@ -15,17 +15,17 @@ import (
 // Config struct used for configuration of app with env variables
 type Config struct {
 	SlackToken         string `envconfig:"SLACK_TOKEN" required:"true"`
-	DatabaseURL        string `envconfig:"DATABASE" required:"true"`
-	HTTPBindAddr       string `envconfig:"HTTP_BIND_ADDR" required:"true"`
-	NotifierInterval   int    `envconfig:"NOTIFIER_INTERVAL" required:"true"`
+	DatabaseURL        string `envconfig:"DATABASE" required:"true" default:"comedian:comedian@/comedian?parseTime=true"`
+	HTTPBindAddr       string `envconfig:"HTTP_BIND_ADDR" required:"true" default:"0.0.0.0:8080"`
+	NotifierInterval   int    `envconfig:"NOTIFIER_INTERVAL" required:"true" default:2`
 	ManagerSlackUserID string `envconfig:"MANAGER_SLACK_USER_ID" required:"true"`
-	ReportTime         string `envconfig:"REPORT_TIME" required:"true"`
-	Language           string `envconfig:"LANGUAGE" required:"false"`
+	ReportTime         string `envconfig:"REPORT_TIME" required:"true" default:"13:05"`
+	Language           string `envconfig:"LANGUAGE" required:"true" default:"en_US"`
 	CollectorURL       string `envconfig:"COLLECTOR_URL" required:"true"`
 	CollectorToken     string `envconfig:"COLLECTOR_TOKEN" required:"true"`
 	ChanGeneral        string `envconfig:"MANAGER_SLACK_CHAN_GENERAL" required:"true"`
-	ReminderRepeatsMax int    `envconfig:"REMINDER_REPEATS_MAX" required:"true"`
-	ReminderTime       int64  `envconfig:"REMINDER_TIME" required:"true"`
+	ReminderRepeatsMax int    `envconfig:"REMINDER_REPEATS_MAX" required:"true" default:5`
+	ReminderTime       int64  `envconfig:"REMINDER_TIME" required:"true" default:5`
 	Debug              bool
 }
 
