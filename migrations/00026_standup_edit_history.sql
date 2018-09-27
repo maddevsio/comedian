960bb86d5d6aec063d4cbc064addad8dcb69df4a
Anatoliy Fedorenko <fedorenko.tolik@gmail.com>
2018-09-27 12:18:20 +0600

    Fix migration

--- migrations/00026_standup_edit_history.sql
+++ migrations/00026_standup_edit_history.sql
@@ -5,5 +5,5 @@ ALTER TABLE `standups` MODIFY `comment` TEXT COLLATE utf8mb4_unicode_ci NOT NULL
 
 -- +goose Down
 -- SQL in this section is executed when the migration is rolled back.
-ALTER TABLE `standup_edit_history` MODIFY `standup_text` VARCHAR(255) NOT NULL;
+ALTER TABLE `standup_edit_history` MODIFY `standup_text` VARCHAR(255) COLLATE utf8mb4_unicode_ci NOT NULL;
 ALTER TABLE `standups` MODIFY `comment` VARCHAR(3000) COLLATE utf8mb4_unicode_ci NOT NULL;
