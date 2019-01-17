2604e72a594ba37f209415c9e021d57a330481db
baitulakova <baitulakova13@gmail.com>
2019-01-17 18:53:06 +0600

    COM-556 add new migration

---
+++ migrations/00032_controll_pannel.sql
@@ -0,0 +1,9 @@
+-- +goose Up
+-- SQL in this section is executed when the migration is applied.
+ALTER TABLE `controll_pannel` ADD `sprint_report_on` BOOLEAN NOT NULL;
+ALTER TABLE `controll_pannel` ADD `sprint_report_time` VARCHAR(255) NOT NULL;
+-- +goose Down
+-- SQL in this section is executed when the migration is rolled back.
+ALTER TABLE `controll_pannel` DROP `sprint_report_on`;
+ALTER TABLE `controll_pannel` DROP `sprint_report_time`;
+
