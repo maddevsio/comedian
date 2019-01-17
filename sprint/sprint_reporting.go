9af8b34117959eadaf9ca45b62d39a7b1744fb40
baitulakova <baitulakova13@gmail.com>
2019-01-17 16:45:49 +0600

    COM-555 changes MakeMessage,add plural translation for days

--- sprint/sprint_reporting.go
+++ sprint/sprint_reporting.go
@@ -1,11 +1,13 @@
 package sprint
 
 import (
+	"errors"
 	"strconv"
 	"strings"
 	"time"
 
 	"github.com/nicksnyder/go-i18n/v2/i18n"
+	"github.com/sirupsen/logrus"
 	"gitlab.com/team-monitoring/comedian/bot"
 )
 
@@ -24,7 +26,7 @@ type ActiveSprint struct {
 	URL                string
 }
 
-func prepareTime(timestring string) (date time.Time) {
+func prepareTime(timestring string) (date time.Time, err error) {
 	//2019-01-14T16:29:02.432+06:00"
 	parts := strings.Split(timestring, "T")
 	var year, month, day int
@@ -35,10 +37,11 @@ func prepareTime(timestring string) (date time.Time) {
 			year, _ = strconv.Atoi(ymt[0])
 			month, _ = strconv.Atoi(ymt[1])
 			day, _ = strconv.Atoi(ymt[2])
+			date = time.Date(year, time.Month(month), day, 0, 0, 0, 0, time.UTC)
+			return date, err
 		}
 	}
-	date = time.Date(year, time.Month(month), day, 0, 0, 0, 0, time.UTC)
-	return date
+	return date, errors.New("Error parsing time")
 }
 
 //countDays return count days between periods
@@ -68,8 +71,14 @@ func MakeActiveSprint(collectorInfo CollectorInfo) ActiveSprint {
 			activeSprint.ResolvedTasksCount++
 		}
 	}
-	startDate := prepareTime(collectorInfo.SprintStart)
-	endDate := prepareTime(collectorInfo.SprintEnd)
+	startDate, err := prepareTime(collectorInfo.SprintStart)
+	if err != nil {
+		logrus.Errorf("sprint.prepareTime failed: %v", err)
+	}
+	endDate, err := prepareTime(collectorInfo.SprintEnd)
+	if err != nil {
+		logrus.Errorf("sprint.prepareTime failed: %v", err)
+	}
 	activeSprint.SprintDaysCount = countDays(startDate, endDate)
 	currentDate := time.Date(time.Now().Year(), time.Now().Month(), time.Now().Day(), 0, 0, 0, 0, time.UTC)
 	activeSprint.PassedDays = countDays(startDate, currentDate)
@@ -95,16 +104,46 @@ func MakeMessage(bot *bot.Bot, activeSprint ActiveSprint) string {
 	sprintProgressPercent += "\n"
 	totalMessage += sprintProgressPercent
 	leftDays := activeSprint.SprintDaysCount - activeSprint.PassedDays
+	sprintTotalDays := localizer.MustLocalize(&i18n.LocalizeConfig{
+		DefaultMessage: &i18n.Message{
+			ID:          "sprintTotalDays",
+			Description: "Displays total days of sprint",
+			One:         "day",
+			Other:       "days",
+		},
+		PluralCount: activeSprint.SprintDaysCount,
+	})
+	sprintPassedDays := localizer.MustLocalize(&i18n.LocalizeConfig{
+		DefaultMessage: &i18n.Message{
+			ID:          "sprintPassedDays",
+			Description: "Displays passed days of sprint",
+			One:         "day",
+			Other:       "days",
+		},
+		PluralCount: activeSprint.PassedDays,
+	})
+	sprintLeftDays := localizer.MustLocalize(&i18n.LocalizeConfig{
+		DefaultMessage: &i18n.Message{
+			ID:          "sprintLeftDays",
+			Description: "Displays left days of sprint",
+			One:         "day",
+			Other:       "days",
+		},
+		PluralCount: leftDays,
+	})
 	sprintDays := localizer.MustLocalize(&i18n.LocalizeConfig{
 		DefaultMessage: &i18n.Message{
 			ID:          "SprintDays",
 			Description: "Displays message about sprint duration",
-			Other:       "Sprint lasts {{.totalDays}} d.,{{.passedDays}} d. passed, {{.leftDays}} d. left.",
+			Other:       "Sprint lasts {{.totalDays}} {{.td}},{{.passedDays}} {{.pd}} passed, {{.leftDays}} {{.ld}} left.",
 		},
 		TemplateData: map[string]interface{}{
 			"totalDays":  activeSprint.SprintDaysCount,
+			"td":         sprintTotalDays,
 			"passedDays": activeSprint.PassedDays,
+			"pd":         sprintPassedDays,
 			"leftDays":   leftDays,
+			"ld":         sprintLeftDays,
 		},
 	})
 	sprintDays += "\n"
@@ -135,6 +174,7 @@ func MakeMessage(bot *bot.Bot, activeSprint ActiveSprint) string {
 		inProgressTask += "\n"
 		totalMessage += inProgressTask
 	}
+	totalMessage += "\n"
 	urlTitle := localizer.MustLocalize(&i18n.LocalizeConfig{
 		DefaultMessage: &i18n.Message{
 			ID:          "UrlTitle",
