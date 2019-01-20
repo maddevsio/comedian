a6222d975db80e0b32cba8c04a7f950ca3dd0e87
baitulakova <baitulakova13@gmail.com>
2019-01-20 23:20:11 +0600

    COM-546 add new migration to controll_pannel

--- migrations/00032_controll_pannel.sql
+++ migrations/00032_controll_pannel.sql
@@ -1,11 +1,13 @@
 -- +goose Up
 -- SQL in this section is executed when the migration is applied.
-ALTER TABLE `controll_pannel` ADD `sprint_report_turn` BOOLEAN NOT NULL;
+ALTER TABLE `controll_pannel` ADD `sprint_report_status` BOOLEAN NOT NULL;
 ALTER TABLE `controll_pannel` ADD `sprint_report_time` VARCHAR(255) NOT NULL;
 ALTER TABLE `controll_pannel` ADD `sprint_report_channel` VARCHAR(255) NOT NULL;
+ALTER TABLE `controll_pannel` ADD `sprint_weekdays` VARCHAR(255) NOT NULL;
 -- +goose Down
 -- SQL in this section is executed when the migration is rolled back.
-ALTER TABLE `controll_pannel` DROP `sprint_report_on`;
+ALTER TABLE `controll_pannel` DROP `sprint_report_status`;
 ALTER TABLE `controll_pannel` DROP `sprint_report_time`;
 ALTER TABLE `controll_pannel` DROP `sprint_report_channel`;
+ALTER TABLE `controll_pannel` DROP `sprint_weekdays`;
 
