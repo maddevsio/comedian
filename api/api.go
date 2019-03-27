d4cec35002054536de2f28bf754588e91d5e3a3c
Anatoliy Fedorenko <fedorenko.tolik@gmail.com>
2019-03-27 13:46:11 +0600

    Add logs to Service Messages

--- api/api.go
+++ api/api.go
@@ -161,16 +161,19 @@ func (api *ComedianAPI) handleServiceMessage(c echo.Context) error {
 
 	body, err := ioutil.ReadAll(c.Request().Body)
 	if err != nil {
+		log.WithFields(log.Fields(map[string]interface{}{"error": err})).Error("handleServiceMessage failed on ReadAll")
 		return err
 	}
 
 	err = json.Unmarshal(body, &incomingEvent)
 	if err != nil {
+		log.WithFields(log.Fields(map[string]interface{}{"error": err})).Error("handleServiceMessage failed on Unmarshal body")
 		return err
 	}
 
 	err = api.comedian.HandleEvent(incomingEvent)
 	if err != nil {
+		log.WithFields(log.Fields(map[string]interface{}{"error": err})).Error("handleServiceMessage failed on HandleEvent")
 		return err
 	}
 
