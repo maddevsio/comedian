2e54af77dd7947ccf0952dc6b990cb21ab541079
Altynai Baitulakova <baitulakova13@gmail.com>
2019-04-18 16:21:46 +0600

    COM-1522 Update migration, add usage of utf8

--- migrations/00045_channel_members.sql
+++ migrations/00045_channel_members.sql
@@ -1,7 +1,7 @@
 -- +goose Up
 -- SQL in this section is executed when the migration is applied.
 ALTER TABLE `channel_members` ADD `real_name` VARCHAR(255) COLLATE utf8mb4_unicode_ci NOT NULL;
-ALTER TABLE `channel_members` ADD `channel_name` VARCHAR(255) NOT NULL;
+ALTER TABLE `channel_members` ADD `channel_name` VARCHAR(255) COLLATE utf8mb4_unicode_ci NOT NULL;
 -- +goose Down
 -- SQL in this section is executed when the migration is rolled back.
 ALTER TABLE `channel_members` DROP `real_name`;
