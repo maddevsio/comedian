a5c4e353a50ee518e485f02108e94f5a3f5402f9
Marat <marat.m2501@gmail.com>
2019-04-18 13:57:48 +0000

    Update entrypoint.sh change env DATABASE

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,5 @@
 dockerize -wait tcp://db:3306 -timeout 90s
 
 echo "Running migrations"
-/goose -dir /migrations mysql $COMEDIAN_DATABASE up
+/goose -dir /migrations mysql $DATABASE up
 /comedian $@
