9b6b7d08379d055be61f7bf418826c4365d24fcf
Anatoliy Fedorenko <fedorenko.tolik@gmail.com>
2019-07-12 13:16:25 +0600

    Add join and leave methods for comedian

--- botuser/standupers.go
+++ botuser/standupers.go
@@ -1,6 +1,7 @@
 package botuser
 
 import (
+	"github.com/maddevsio/comedian/model"
 	"github.com/nicksnyder/go-i18n/v2/i18n"
 	"github.com/nlopes/slack"
 	log "github.com/sirupsen/logrus"
@@ -8,7 +9,76 @@ import (
 )
 
 func (bot *Bot) joinCommand(command slack.SlashCommand) string {
-	return ""
+	_, err := bot.db.FindStansuperByUserID(command.UserID, command.ChannelID)
+	if err == nil {
+		youAlreadyStandup, err := bot.localizer.Localize(&i18n.LocalizeConfig{
+			DefaultMessage: &i18n.Message{
+				ID:    "youAlreadyStandup",
+				Other: "You already a part of standup team",
+			},
+		})
+		if err != nil {
+			log.Error(err)
+		}
+		return youAlreadyStandup
+	}
+
+	_, err = bot.db.CreateStanduper(model.Standuper{})
+	if err != nil {
+		createStanduperFailed, err := bot.localizer.Localize(&i18n.LocalizeConfig{
+			DefaultMessage: &i18n.Message{
+				ID:    "createStanduperFailed",
+				Other: "Could not add you to standup team",
+			},
+		})
+		if err != nil {
+			log.Error(err)
+		}
+		log.Error("CreateStanduper failed: ", err)
+		return createStanduperFailed
+	}
+
+	channel, err := bot.db.SelectChannel(command.ChannelID)
+	if err != nil {
+		selectChannelFailed, err := bot.localizer.Localize(&i18n.LocalizeConfig{
+			DefaultMessage: &i18n.Message{
+				ID:    "selectChannelFailed",
+				Other: "Could not add you to standup team, try kick and re-add me to the channel",
+			},
+		})
+		if err != nil {
+			log.Error(err)
+		}
+		log.Error("SelectChannel failed: ", err)
+		return selectChannelFailed
+	}
+
+	if channel.StandupTime == "" {
+		welcomeWithNoDeadline, err := bot.localizer.Localize(&i18n.LocalizeConfig{
+			DefaultMessage: &i18n.Message{
+				ID:    "welcomeNoDedline",
+				Other: "Welcome to the standup team, no standup deadline has been setup yet",
+			},
+		})
+		if err != nil {
+			log.Error(err)
+		}
+		return welcomeWithNoDeadline
+	}
+
+	welcomeWithDeadline, err := bot.localizer.Localize(&i18n.LocalizeConfig{
+		DefaultMessage: &i18n.Message{
+			ID:    "welcomeWithDedline",
+			Other: "Welcome to the standup team, please, submit your standups no later than {{.Deadline}}",
+		},
+		TemplateData: map[string]interface{}{
+			"Deadline": channel.StandupTime,
+		},
+	})
+	if err != nil {
+		log.Error(err)
+	}
+	return welcomeWithDeadline
 }
 
 func (bot *Bot) showCommand(command slack.SlashCommand) string {
@@ -48,5 +118,43 @@ func (bot *Bot) showCommand(command slack.SlashCommand) string {
 }
 
 func (bot *Bot) quitCommand(command slack.SlashCommand) string {
-	return ""
+	standuper, err := bot.db.FindStansuperByUserID(command.UserID, command.ChannelID)
+	if err != nil {
+		notStanduper, err := bot.localizer.Localize(&i18n.LocalizeConfig{
+			DefaultMessage: &i18n.Message{
+				ID:    "notStanduper",
+				Other: "You do not standup yet",
+			},
+		})
+		if err != nil {
+			log.Error(err)
+		}
+		return notStanduper
+	}
+
+	err = bot.db.DeleteStanduper(standuper.ID)
+	if err != nil {
+		log.Error("DeleteStanduper failed: ", err)
+		failedLeaveStandupers, err := bot.localizer.Localize(&i18n.LocalizeConfig{
+			DefaultMessage: &i18n.Message{
+				ID:    "failedLeaveStandupers",
+				Other: "Could not remove you from standup team",
+			},
+		})
+		if err != nil {
+			log.Error(err)
+		}
+		return failedLeaveStandupers
+	}
+
+	leaveStanupers, err := bot.localizer.Localize(&i18n.LocalizeConfig{
+		DefaultMessage: &i18n.Message{
+			ID:    "leaveStanupers",
+			Other: "You no longer have to submit standups, thanks for all your standups and messages",
+		},
+	})
+	if err != nil {
+		log.Error(err)
+	}
+	return leaveStanupers
 }
